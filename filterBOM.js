15a0c1c980f9a1a8bfa1a843799a4f86c1e10c9a
Erich Smith <erich.smith@jupiterone.com>
2021-04-30 15:37:52 -0400

    Initial commit

---
+++ filterBOM.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+function uniqueComponents(components) {
+  const uniques = [];
+  seen = {};
+  for (const component of components) {
+    seen[component.purl] = component;
+  }
+  for (const purl in seen) {
+    uniques.push(seen[purl]);
+  }
+
+  return uniques;
+}
+
+async function run() {
+  const infile = process.argv[2];
+  if (!infile) {
+    console.error("Missing input file argument!");
+    process.exit(2);
+  }
+  const outfile = process.argv[3] || infile;
+
+  const data = require(infile);
+  data.components = uniqueComponents(data.components);
+  fs.writeFileSync(outfile, JSON.stringify(data, null, 2));
+}
+
+run().catch(console.error);
