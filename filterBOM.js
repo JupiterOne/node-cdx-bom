cb4d39d9d89bdde5faac8076adf7aa82b9a5aacf
Erich Smith <erich.smith@jupiterone.com>
2021-05-05 15:53:06 -0400

    Clarify usage of scope property

--- filterBOM.js
+++ filterBOM.js
@@ -1,17 +1,19 @@
 const fs = require('fs');
 
-function uniqueComponents(components) {
-  const uniques = [];
+function uniqueRequiredComponents(components) {
   seen = {};
   for (const component of components) {
-    component.scope = "required";
-    seen[component.purl] = component;
+    seen[component.purl] = {
+      ...component,
+      scope: 'required'
+      // 'scope' is useful for downstream consumers of the SBOM.
+      // Given the assumptions of this project we have enough knowledge
+      // to set this value here.
+      // May be one of 'optional', 'required', 'excluded'.
+      // See: https://github.com/CycloneDX/specification/blob/master/schema/bom-1.2.xsd#L487-L510
+    };
   }
-  for (const purl in seen) {
-    uniques.push(seen[purl]);
-  }
-
-  return uniques;
+  return Object.values(seen);
 }
 
 async function run() {
@@ -24,7 +26,7 @@ async function run() {
 
   const data = require(infile);
   console.log('Before: ' + data.components.length + ' components.');
-  data.components = uniqueComponents(data.components);
+  data.components = uniqueRequiredComponents(data.components);
   console.log(' After: ' + data.components.length + ' components.');
   fs.writeFileSync(outfile, JSON.stringify(data, null, 2));
 }
